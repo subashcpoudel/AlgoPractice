b7100d80a8bb7f35541775a55cdd3cc1b835c9d6
SubashPoudel <subash@subashchandra.com>
2021-05-28 11:38:13 +0545

    Self Version from Scratch

--- linkedlist.py
+++ linkedlist.py
@@ -1,86 +1,107 @@
-# Linked list example
+from typing import ByteString
 
-
-# the Node class
+#Node Class
 class Node(object):
     def __init__(self, val):
-        self.val = val
-        self.next = None
+        self.val=val
+        self.next=None
 
     def get_data(self):
         return self.val
 
-    def set_data(self, val):
-        self.val = val
+    def set_data(self, data):
+        self.val=data
 
     def get_next(self):
         return self.next
 
     def set_next(self, next):
-        self.next = next
+        self.next=next
 
-
-# the LinkedList class
+#List Class
 class LinkedList(object):
     def __init__(self, head=None):
-        self.head = head
-        self.count = 0
+        self.head=head
+        self.count=0
 
     def get_count(self):
         return self.count
 
     def insert(self, data):
-        new_node = Node(data)
+        new_node=Node(data)
         new_node.set_next(self.head)
-        self.head = new_node
-        self.count += 1
-
-    def find(self, val):
-        item = self.head
-        while (item != None):
+        self.head=new_node
+        self.count+=1
+    
+    def find(self,val):
+        item=self.head
+        pos=0
+        while(item!=None):
             if item.get_data() == val:
-                return item
+                return pos
             else:
-                item = item.get_next()
+                pos+=1
+                item=item.get_next()
         return None
 
-    def deleteAt(self, idx):
-        if idx > self.count:
-            return
-        if self.head == None:
-            return
-        else:
-            tempIdx = 0
-            node = self.head
-            while tempIdx < idx-1:
-                node = node.get_next()
-                tempIdx += 1
-            node.set_next(node.get_next().get_next())
-            self.count -= 1
+    def deleteAt(self,at):
+        #If Delete at is larger than List count deletes last node
+        item=self.head
+        cur=Node()
+        pos=0
+        while(at!=pos):
+            cur=item
+            item=item.get_next()
+            pos+=1
+        cur.set_next=item.get_next
+        self.count-=1
+    
+    def dump(self):
+        item=self.head
+        pos=0
+        while(item!=None):
+            print("Node pos & value:",pos,item.val)
+            pos+=1
+            item=item.get_next()
 
-    def dump_list(self):
-        tempnode = self.head
-        while (tempnode != None):
-            print("Node: ", tempnode.get_data())
-            tempnode = tempnode.get_next()
+#Initialize Lists and Start Program
+def initprog(a,b):
+    a=LinkedList()
+    b=LinkedList()
+    progstart(a,b)
+    
+#Actual Start Program
+def progstart(a,b):
+    x=input("\n Select Linked list: a or b? or c to destroy both ")
+    if(x=="a"):
+        compstart(a,b)
+    elif(x=="b"):
+        compstart(b,a)
+    elif(x=="c"):
+        initprog(a,b)
+    else:
+        progstart(a,b)
 
+#Actual Computations for Program
+def compstart(list,another):
+    x=input("\n Select Insert-1, Find-2, DeleteAt-3, Dump-4,Start-5 ")
+    if(x=="1"):
+        data=input("\n Data to insert ")
+        list.insert(data)
+        print("\n Data inserted New count = ",list.get_count())
+    elif(x=="2"):
+        data=input("Find what? ")
+        print("\n Found at",list.find(data))
+    elif(x=="3"):
+        data=input("Delete At? ")
+        Print("\n Data Deleted New Count = ",list.get_count())
+    elif(x=="4"):
+        list.dump()
+    elif(x=="5"):
+        progstart(list,another)
+    compstart(list, another)
 
-# create a linked list and insert some items
-itemlist = LinkedList()
-itemlist.insert(38)
-itemlist.insert(49)
-itemlist.insert(13)
-itemlist.insert(15)
-
-itemlist.dump_list()
-
-# exercise the list
-print("Item count: ", itemlist.get_count())
-print("Finding item: ", itemlist.find(13))
-print("Finding item: ", itemlist.find(78))
-
-# delete an item
-itemlist.deleteAt(3)
-print("Item count: ", itemlist.get_count())
-print("Finding item: ", itemlist.find(38))
-itemlist.dump_list()
+#Code Initial Functions     
+a=LinkedList()
+b=LinkedList()
+progstart(a,b)
